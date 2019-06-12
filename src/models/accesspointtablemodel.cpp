81ffa5888861e269b8fda0671111af24275149c4
Zach Leytus <zach.leytus@pm.me>
2019-06-12 11:21:21 -0700

    Add models for access point table view
    This adds a subclass of QAbstractTableModel that can provide data to
    QTableViews displaying access points.

---
+++ src/models/accesspointtablemodel.cpp
@@ -0,0 +1,80 @@
+#include "accesspointtablemodel.h"
+
+#include <QAbstractTableModel>
+#include <QVariant>
+#include <QVector>
+
+using namespace KawaiiFi;
+
+AccessPointTableModel::AccessPointTableModel(QObject *parent) : QAbstractTableModel(parent)
+{
+}
+
+void AccessPointTableModel::updateAccessPoints(QVector<KawaiiFi::AccessPoint> accessPoints)
+{
+	_accessPoints = accessPoints;
+}
+
+int AccessPointTableModel::rowCount(const QModelIndex &parent) const
+{
+	return _accessPoints.size();
+}
+
+int AccessPointTableModel::columnCount(const QModelIndex &parent) const
+{
+	return 6;
+}
+
+QVariant AccessPointTableModel::data(const QModelIndex &index, int role) const
+{
+	if (!index.isValid()) {
+		return QVariant();
+	}
+
+	if (role == Qt::DisplayRole) {
+		const AccessPoint &ap = _accessPoints[index.row()];
+
+		switch (index.column()) {
+		case 0:
+			return ap.ssid();
+		case 1:
+			return ap.bssid();
+		case 2:
+			return ap.vendor();
+		case 3:
+			return ap.channel();
+		case 4:
+			return ap.channelWidth();
+		case 5:
+			return ap.signalStrength();
+		}
+	}
+
+	return QVariant();
+}
+
+QVariant AccessPointTableModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+	if (role != Qt::DisplayRole) {
+		return QVariant();
+	}
+
+	if (orientation == Qt::Orientation::Horizontal) {
+		switch (section) {
+		case 0:
+			return tr("Network Name");
+		case 1:
+			return tr("BSSID");
+		case 2:
+			return tr("Vendor");
+		case 3:
+			return tr("Channel");
+		case 4:
+			return tr("Channel Width");
+		case 5:
+			return tr("Signal Strength");
+		}
+	}
+
+	return QVariant();
+}
---
+++ src/models/accesspointtablemodel.h
@@ -0,0 +1,26 @@
+#ifndef ACCESSPOINTTABLEMODEL_H
+#define ACCESSPOINTTABLEMODEL_H
+
+#include "accesspoint.h"
+
+#include <QAbstractTableModel>
+#include <QVariant>
+#include <QVector>
+
+namespace KawaiiFi {
+	class AccessPointTableModel : public QAbstractTableModel {
+		Q_OBJECT
+	public:
+		AccessPointTableModel(QObject *parent = nullptr);
+		void updateAccessPoints(QVector<AccessPoint> accessPoints);
+		int rowCount(const QModelIndex &parent) const override;
+		int columnCount(const QModelIndex &parent) const override;
+		QVariant data(const QModelIndex &index, int role) const override;
+		QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
+
+	private:
+		QVector<AccessPoint> _accessPoints;
+	};
+} // namespace KawaiiFi
+
+#endif // ACCESSPOINTTABLEMODEL_H
