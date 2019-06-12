e37128cfb427fea55f5a94f763f32064cca67eba
Zach Leytus <zach.leytus@pm.me>
2019-06-11 22:12:01 -0700

    Initiate and handle dummy scans in main window

--- src/ui/mainwindow.cpp
+++ src/ui/mainwindow.cpp
@@ -1,14 +1,63 @@
 #include "mainwindow.h"
+#include "models/accesspoint.h"
+#include "models/wirelessinterface.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+#include <QComboBox>
+#include <QFuture>
+#include <QFutureWatcher>
+#include <QMainWindow>
+#include <QProgressBar>
+#include <QTimer>
+#include <QVector>
+#include <QtConcurrent/QtConcurrentRun>
+
+using namespace KawaiiFi;
+
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), _ui(new Ui::MainWindow)
 {
-	ui->setupUi(this);
+	_ui->setupUi(this);
+	_ui->scanIntervalComboBox->addItems(QStringList({"10 seconds", "30 seconds", "1 minute"}));
+
+	_wirelessInterfaces = KawaiiFi::getWirelessInterfaces();
+	for (const auto &interface : _wirelessInterfaces) {
+		_ui->interfaceComboBox->addItem(interface.name());
+	}
+
+	_ui->apSpectrumChartView->chart()->layout()->setContentsMargins(0, 0, 0, 0);
+
+	_ui->splitter->setSizes({INT_MAX, INT_MAX});
+
+	connect(&_scanResultWatcher, &QFutureWatcher<QVector<KawaiiFi::AccessPoint>>::finished, this,
+	        &MainWindow::handleScanResult);
+
+	_progressBar = new QProgressBar();
+	_progressBar->setRange(0, 0);
+	statusBar()->addPermanentWidget(_progressBar);
+	_progressBar->hide();
+	scan();
 }
 
 MainWindow::~MainWindow()
 {
-	delete ui;
+	delete _ui;
+}
+
+void MainWindow::scan()
+{
+	if (_wirelessInterfaces.size() > 0) {
+		_progressBar->show();
+		QFuture<QVector<KawaiiFi::AccessPoint>> scanResult =
+		        QtConcurrent::run(_wirelessInterfaces[_ui->interfaceComboBox->currentIndex()],
+		                &KawaiiFi::WirelessInterface::waitForNewScanResults);
+		_scanResultWatcher.setFuture(scanResult);
+	}
+}
+
+void MainWindow::handleScanResult()
+{
+	_progressBar->hide();
+	statusBar()->showMessage(
+	        QString("Found %0 access points").arg(_scanResultWatcher.result().size()));
+	QTimer::singleShot(5000, this, &MainWindow::scan);
 }
--- src/ui/mainwindow.h
+++ src/ui/mainwindow.h
@@ -1,22 +1,32 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include "models/accesspoint.h"
+#include "models/wirelessinterface.h"
+
+#include <QFutureWatcher>
 #include <QMainWindow>
+#include <QProgressBar>
 
-namespace Ui {
-	class MainWindow;
-}
+namespace KawaiiFi {
+	namespace Ui {
+		class MainWindow;
+	}
 
-class MainWindow : public QMainWindow
-{
-	Q_OBJECT
+	class MainWindow : public QMainWindow {
+		Q_OBJECT
+	public:
+		explicit MainWindow(QWidget *parent = nullptr);
+		~MainWindow();
 
-public:
-	explicit MainWindow(QWidget *parent = nullptr);
-	~MainWindow();
-
-private:
-	Ui::MainWindow *ui;
-};
+	private:
+		void scan();
+		void handleScanResult();
+		Ui::MainWindow *_ui;
+		QProgressBar *_progressBar;
+		QFutureWatcher<QVector<KawaiiFi::AccessPoint>> _scanResultWatcher;
+		QVector<KawaiiFi::WirelessInterface> _wirelessInterfaces;
+	};
+} // namespace KawaiiFi
 
 #endif // MAINWINDOW_H
