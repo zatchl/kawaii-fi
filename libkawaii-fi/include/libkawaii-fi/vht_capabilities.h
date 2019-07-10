b7cadfeace4433d94aec43030fcc00aea98baa07
Zach Leytus <zach.leytus@pm.me>
2019-07-09 17:33:48 -0700

    Create a type to represent a VHT Capability IE

---
+++ libkawaii-fi/include/libkawaii-fi/vht_capabilities.h
@@ -0,0 +1,78 @@
+#ifndef VHT_CAPABILITIES_H
+#define VHT_CAPABILITIES_H
+
+#include <QDBusArgument>
+#include <QMetaType>
+#include <QVector>
+
+enum class VhtSupportedChannelWidth {
+	NoOneSixtyMhzNoEightyPlusEightyMhz,
+	OneSixtyMhz,
+	OneSixtyMhzEightyPlusEightyMhz
+};
+
+enum class VhtTxStbc { Disabled, Enabled };
+
+enum class VhtRxStbc {
+	Disabled,
+	OneSpatialStream,
+	TwoSpatialStreams,
+	ThreeSpatialStreams,
+	FourSpatialStreams
+};
+
+enum class LinkAdaptation { NotSupported, SupportedUnsolicited, SupportedResponseUnsolicited };
+
+enum class VhtMcs { NotSupported, OneThroughSeven, OneThroughEight, OneThroughNine };
+
+struct VhtCapabilities {
+	bool supported = false;
+	unsigned int max_mpdu_length = 0;
+	VhtSupportedChannelWidth supported_channel_width = VhtSupportedChannelWidth::OneSixtyMhz;
+	bool rx_ldpc = false;
+	bool short_gi_80_mhz = false;
+	bool short_gi_160_mhz = false;
+	VhtTxStbc tx_stbc = VhtTxStbc::Disabled;
+	VhtRxStbc rx_stbc = VhtRxStbc::Disabled;
+	bool su_beamformer_capable = false;
+	bool su_beamformee_capable = false;
+	unsigned int beamformee_sts = 1;
+	unsigned int sounding_dimensions = 1;
+	bool mu_beamformer_capable = false;
+	bool mu_beamformee_capable = false;
+	bool vht_txop_ps = false;
+	bool htc_vht_capable = false;
+	unsigned int max_ampdu_length = 0;
+	LinkAdaptation link_adaptation = LinkAdaptation::NotSupported;
+	bool rx_antenna_pattern_consistent = false;
+	bool tx_antenna_pattern_consistent = false;
+	VhtMcs mcs_rx_one_ss = VhtMcs::NotSupported;
+	VhtMcs mcs_rx_two_ss = VhtMcs::NotSupported;
+	VhtMcs mcs_rx_three_ss = VhtMcs::NotSupported;
+	VhtMcs mcs_rx_four_ss = VhtMcs::NotSupported;
+	VhtMcs mcs_rx_five_ss = VhtMcs::NotSupported;
+	VhtMcs mcs_rx_six_ss = VhtMcs::NotSupported;
+	VhtMcs mcs_rx_seven_ss = VhtMcs::NotSupported;
+	VhtMcs mcs_rx_eight_ss = VhtMcs::NotSupported;
+	unsigned int rx_highest_long_gi_data_rate = 0;
+	unsigned int max_nsts_total = 0;
+	VhtMcs mcs_tx_one_ss = VhtMcs::NotSupported;
+	VhtMcs mcs_tx_two_ss = VhtMcs::NotSupported;
+	VhtMcs mcs_tx_three_ss = VhtMcs::NotSupported;
+	VhtMcs mcs_tx_four_ss = VhtMcs::NotSupported;
+	VhtMcs mcs_tx_five_ss = VhtMcs::NotSupported;
+	VhtMcs mcs_tx_six_ss = VhtMcs::NotSupported;
+	VhtMcs mcs_tx_seven_ss = VhtMcs::NotSupported;
+	VhtMcs mcs_tx_eight_ss = VhtMcs::NotSupported;
+	unsigned int tx_highest_long_gi_data_rate = 0;
+	bool extended_nss_bw_capable = false;
+};
+Q_DECLARE_METATYPE(VhtCapabilities)
+
+// Marshall the VHT Capability data into a D-Bus argument
+QDBusArgument &operator<<(QDBusArgument &argument, const VhtCapabilities &vht_cap);
+
+// Retrieve the VHT Capability data from the D-Bus argument
+const QDBusArgument &operator>>(const QDBusArgument &argument, VhtCapabilities &vht_cap);
+
+#endif // VHT_CAPABILITIES_H
---
+++ libkawaii-fi/src/vht_capabilities.cpp
@@ -0,0 +1,91 @@
+#include "libkawaii-fi/vht_capabilities.h"
+
+#include <QDBusArgument>
+
+QDBusArgument &operator<<(QDBusArgument &argument, const VhtCapabilities &vht_cap)
+{
+	argument.beginStructure();
+	argument << vht_cap.supported << vht_cap.max_mpdu_length
+	         << static_cast<int>(vht_cap.supported_channel_width) << vht_cap.rx_ldpc
+	         << vht_cap.short_gi_80_mhz << vht_cap.short_gi_160_mhz
+	         << static_cast<int>(vht_cap.tx_stbc) << static_cast<int>(vht_cap.rx_stbc)
+	         << vht_cap.su_beamformer_capable << vht_cap.su_beamformee_capable
+	         << vht_cap.beamformee_sts << vht_cap.sounding_dimensions
+	         << vht_cap.mu_beamformer_capable << vht_cap.mu_beamformee_capable
+	         << vht_cap.vht_txop_ps << vht_cap.htc_vht_capable << vht_cap.max_ampdu_length
+	         << static_cast<int>(vht_cap.link_adaptation) << vht_cap.rx_antenna_pattern_consistent
+	         << vht_cap.tx_antenna_pattern_consistent << static_cast<int>(vht_cap.mcs_rx_one_ss)
+	         << static_cast<int>(vht_cap.mcs_rx_two_ss) << static_cast<int>(vht_cap.mcs_rx_three_ss)
+	         << static_cast<int>(vht_cap.mcs_rx_four_ss) << static_cast<int>(vht_cap.mcs_rx_five_ss)
+	         << static_cast<int>(vht_cap.mcs_rx_six_ss) << static_cast<int>(vht_cap.mcs_rx_seven_ss)
+	         << static_cast<int>(vht_cap.mcs_rx_eight_ss) << vht_cap.rx_highest_long_gi_data_rate
+	         << vht_cap.max_nsts_total << static_cast<int>(vht_cap.mcs_tx_one_ss)
+	         << static_cast<int>(vht_cap.mcs_tx_two_ss) << static_cast<int>(vht_cap.mcs_tx_three_ss)
+	         << static_cast<int>(vht_cap.mcs_tx_four_ss) << static_cast<int>(vht_cap.mcs_tx_five_ss)
+	         << static_cast<int>(vht_cap.mcs_tx_six_ss) << static_cast<int>(vht_cap.mcs_tx_seven_ss)
+	         << static_cast<int>(vht_cap.mcs_tx_eight_ss) << vht_cap.tx_highest_long_gi_data_rate
+	         << vht_cap.extended_nss_bw_capable;
+	argument.endStructure();
+	return argument;
+}
+
+const QDBusArgument &operator>>(const QDBusArgument &argument, VhtCapabilities &vht_cap)
+{
+	int supported_channel_width = 0;
+	int tx_stbc = 0;
+	int rx_stbc = 0;
+	int link_adaptation = 0;
+	int mcs_rx_one_ss = 0;
+	int mcs_rx_two_ss = 0;
+	int mcs_rx_three_ss = 0;
+	int mcs_rx_four_ss = 0;
+	int mcs_rx_five_ss = 0;
+	int mcs_rx_six_ss = 0;
+	int mcs_rx_seven_ss = 0;
+	int mcs_rx_eight_ss = 0;
+	int mcs_tx_one_ss = 0;
+	int mcs_tx_two_ss = 0;
+	int mcs_tx_three_ss = 0;
+	int mcs_tx_four_ss = 0;
+	int mcs_tx_five_ss = 0;
+	int mcs_tx_six_ss = 0;
+	int mcs_tx_seven_ss = 0;
+	int mcs_tx_eight_ss = 0;
+	argument.beginStructure();
+	argument >> vht_cap.supported >> vht_cap.max_mpdu_length >> supported_channel_width >>
+	        vht_cap.rx_ldpc >> vht_cap.short_gi_80_mhz >> vht_cap.short_gi_160_mhz >> tx_stbc >>
+	        rx_stbc >> vht_cap.su_beamformer_capable >> vht_cap.su_beamformee_capable >>
+	        vht_cap.beamformee_sts >> vht_cap.sounding_dimensions >>
+	        vht_cap.mu_beamformer_capable >> vht_cap.mu_beamformee_capable >> vht_cap.vht_txop_ps >>
+	        vht_cap.htc_vht_capable >> vht_cap.max_ampdu_length >> link_adaptation >>
+	        vht_cap.rx_antenna_pattern_consistent >> vht_cap.tx_antenna_pattern_consistent >>
+	        mcs_rx_one_ss >> mcs_rx_two_ss >> mcs_rx_three_ss >> mcs_rx_four_ss >> mcs_rx_five_ss >>
+	        mcs_rx_six_ss >> mcs_rx_seven_ss >> mcs_rx_eight_ss >>
+	        vht_cap.rx_highest_long_gi_data_rate >> vht_cap.max_nsts_total >> mcs_tx_one_ss >>
+	        mcs_tx_two_ss >> mcs_tx_three_ss >> mcs_tx_four_ss >> mcs_tx_five_ss >> mcs_tx_six_ss >>
+	        mcs_tx_seven_ss >> mcs_tx_eight_ss >> vht_cap.tx_highest_long_gi_data_rate >>
+	        vht_cap.extended_nss_bw_capable;
+	argument.endStructure();
+	vht_cap.supported_channel_width =
+	        static_cast<VhtSupportedChannelWidth>(supported_channel_width);
+	vht_cap.tx_stbc = static_cast<VhtTxStbc>(tx_stbc);
+	vht_cap.rx_stbc = static_cast<VhtRxStbc>(rx_stbc);
+	vht_cap.link_adaptation = static_cast<LinkAdaptation>(link_adaptation);
+	vht_cap.mcs_rx_one_ss = static_cast<VhtMcs>(mcs_rx_one_ss);
+	vht_cap.mcs_rx_two_ss = static_cast<VhtMcs>(mcs_rx_two_ss);
+	vht_cap.mcs_rx_three_ss = static_cast<VhtMcs>(mcs_rx_three_ss);
+	vht_cap.mcs_rx_four_ss = static_cast<VhtMcs>(mcs_rx_four_ss);
+	vht_cap.mcs_rx_five_ss = static_cast<VhtMcs>(mcs_rx_five_ss);
+	vht_cap.mcs_rx_six_ss = static_cast<VhtMcs>(mcs_rx_six_ss);
+	vht_cap.mcs_rx_seven_ss = static_cast<VhtMcs>(mcs_rx_seven_ss);
+	vht_cap.mcs_rx_eight_ss = static_cast<VhtMcs>(mcs_rx_eight_ss);
+	vht_cap.mcs_tx_one_ss = static_cast<VhtMcs>(mcs_tx_one_ss);
+	vht_cap.mcs_tx_two_ss = static_cast<VhtMcs>(mcs_tx_two_ss);
+	vht_cap.mcs_tx_three_ss = static_cast<VhtMcs>(mcs_tx_three_ss);
+	vht_cap.mcs_tx_four_ss = static_cast<VhtMcs>(mcs_tx_four_ss);
+	vht_cap.mcs_tx_five_ss = static_cast<VhtMcs>(mcs_tx_five_ss);
+	vht_cap.mcs_tx_six_ss = static_cast<VhtMcs>(mcs_tx_six_ss);
+	vht_cap.mcs_tx_seven_ss = static_cast<VhtMcs>(mcs_tx_seven_ss);
+	vht_cap.mcs_tx_eight_ss = static_cast<VhtMcs>(mcs_tx_eight_ss);
+	return argument;
+}
