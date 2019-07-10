0610125f923d4dd65cab9a9def691cf916c947bb
Zach Leytus <zach.leytus@pm.me>
2019-07-09 18:56:21 -0700

    Remove unused function

--- kawaii-fi-server/src/server/netlink/nl_wifi_scan.cpp
+++ kawaii-fi-server/src/server/netlink/nl_wifi_scan.cpp
@@ -15,23 +15,6 @@
 #include <linux/nl80211.h>
 
 namespace {
-	// From linux/net/wireless/util.c
-	int ieee80211_frequency_to_channel(int freq)
-	{
-		/* see 802.11 17.3.8.3.2 and Annex J */
-		if (freq == 2484)
-			return 14;
-		else if (freq < 2484)
-			return (freq - 2407) / 5;
-		else if (freq >= 4910 && freq <= 4980)
-			return (freq - 4000) / 5;
-		else if (freq <= 45000) /* DMG band lower limit */
-			return (freq - 5000) / 5;
-		else if (freq >= 58320 && freq <= 70200)
-			return (freq - 56160) / 2160;
-		else
-			return 0;
-	}
 
 	int process_access_point(nl_msg *msg, void *aps)
 	{
