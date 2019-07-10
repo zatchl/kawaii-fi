ea5da4f90f2309ba30e6ac9937c3c95f717aed99
Zach Leytus <zach.leytus@pm.me>
2019-07-09 17:31:40 -0700

    Create a type to represent an HT Operation IE

---
+++ libkawaii-fi/include/libkawaii-fi/ht_operations.h
@@ -0,0 +1,48 @@
+#ifndef HT_OPERATIONS_H
+#define HT_OPERATIONS_H
+
+#include "ht_capabilities.h"
+
+#include <QDBusArgument>
+#include <QMetaType>
+#include <QVector>
+
+enum class SecondaryChannelOffset { NoSecondaryChannel, Above, Below };
+
+enum class HtProtection { None, Nonmember, TwentyMhz, NonHtMixed };
+
+enum class PcoPhase { TwentyMhz, FourtyMhz };
+
+struct HtOperations {
+	bool supported;
+	unsigned int primary_channel;
+	SecondaryChannelOffset secondary_channel_offset = SecondaryChannelOffset::NoSecondaryChannel;
+	HtSupportedChannelWidth supported_channel_width = HtSupportedChannelWidth::TwentyMhz;
+	bool rifs = false;
+	HtProtection ht_protection = HtProtection::None;
+	bool non_greenfield_stas_present = false;
+	bool obss_non_ht_stas_present = false;
+	unsigned int center_freq_segment_two = 0;
+	bool dual_beacon = false;
+	bool dual_cts_protection = false;
+	bool stbc_beacon = false;
+	bool lsig_txop_protection_full_support = false;
+	bool pco_active = false;
+	PcoPhase pco_phase = PcoPhase::TwentyMhz;
+
+	QVector<uint8_t> rx_mcs;
+	unsigned int highest_supported_data_rate = 0;
+	bool tx_mcs_defined = false;
+	bool tx_rx_mcs_equal = true;
+	unsigned int max_tx_spatial_streams = 0;
+	bool tx_unequal_modulation = false;
+};
+Q_DECLARE_METATYPE(HtOperations)
+
+// Marshall the HT Operation data into a D-Bus argument
+QDBusArgument &operator<<(QDBusArgument &argument, const HtOperations &ht_op);
+
+// Retrieve the HT Operation data from the D-Bus argument
+const QDBusArgument &operator>>(const QDBusArgument &argument, HtOperations &ht_op);
+
+#endif // HT_OPERATIONS_H
---
+++ libkawaii-fi/src/ht_operations.cpp
@@ -0,0 +1,41 @@
+#include "libkawaii-fi/ht_operations.h"
+
+QDBusArgument &operator<<(QDBusArgument &argument, const HtOperations &ht_op)
+{
+	argument.beginStructure();
+	argument << ht_op.supported << ht_op.primary_channel
+	         << static_cast<int>(ht_op.secondary_channel_offset)
+	         << static_cast<int>(ht_op.supported_channel_width) << ht_op.rifs
+	         << static_cast<int>(ht_op.ht_protection) << ht_op.non_greenfield_stas_present
+	         << ht_op.obss_non_ht_stas_present << ht_op.center_freq_segment_two << ht_op.dual_beacon
+	         << ht_op.dual_cts_protection << ht_op.stbc_beacon
+	         << ht_op.lsig_txop_protection_full_support << ht_op.pco_active
+	         << static_cast<int>(ht_op.pco_phase) << ht_op.rx_mcs
+	         << ht_op.highest_supported_data_rate << ht_op.tx_mcs_defined << ht_op.tx_rx_mcs_equal
+	         << ht_op.max_tx_spatial_streams << ht_op.tx_unequal_modulation;
+	argument.endStructure();
+	return argument;
+}
+
+const QDBusArgument &operator>>(const QDBusArgument &argument, HtOperations &ht_op)
+{
+	int secondary_channel_offset = 0;
+	int supported_channel_width = 0;
+	int ht_protection = 0;
+	int pco_phase = 0;
+	argument.beginStructure();
+	argument >> ht_op.supported >> ht_op.primary_channel >> secondary_channel_offset >>
+	        supported_channel_width >> ht_op.rifs >> ht_protection >>
+	        ht_op.non_greenfield_stas_present >> ht_op.obss_non_ht_stas_present >>
+	        ht_op.center_freq_segment_two >> ht_op.dual_beacon >> ht_op.dual_cts_protection >>
+	        ht_op.stbc_beacon >> ht_op.lsig_txop_protection_full_support >> ht_op.pco_active >>
+	        pco_phase >> ht_op.rx_mcs >> ht_op.highest_supported_data_rate >>
+	        ht_op.tx_mcs_defined >> ht_op.tx_rx_mcs_equal >> ht_op.max_tx_spatial_streams >>
+	        ht_op.tx_unequal_modulation;
+	argument.endStructure();
+	ht_op.secondary_channel_offset = static_cast<SecondaryChannelOffset>(secondary_channel_offset);
+	ht_op.supported_channel_width = static_cast<HtSupportedChannelWidth>(supported_channel_width);
+	ht_op.ht_protection = static_cast<HtProtection>(ht_protection);
+	ht_op.pco_phase = static_cast<PcoPhase>(pco_phase);
+	return argument;
+}
