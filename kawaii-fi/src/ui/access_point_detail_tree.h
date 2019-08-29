6c94b77ba85bf16540aa345d61bc476fb4bdf8b0
Zach Leytus <zach.leytus@pm.me>
2019-08-29 01:35:33 -0700

    Create QTreeView for displaying IEs

---
+++ kawaii-fi/src/ui/access_point_detail_tree.h
@@ -0,0 +1,29 @@
+#ifndef ACCESS_POINT_DETAIL_TREE_H
+#define ACCESS_POINT_DETAIL_TREE_H
+
+#include <QByteArrayData>
+#include <QStandardItemModel>
+#include <QString>
+#include <QTreeView>
+
+class AccessPoint;
+class AccessPointScanner;
+class QObject;
+class QWidget;
+
+class AccessPointDetailTreeView : public QTreeView {
+	Q_OBJECT
+public:
+	AccessPointDetailTreeView(QWidget *parent = nullptr);
+
+	void show_ies(const QString &bssid);
+	void set_ap_scanner(const AccessPointScanner *ap_scanner);
+
+private:
+	void show_ies_for_ap(const AccessPoint &ap);
+
+	QStandardItemModel *const model_ = new QStandardItemModel(this);
+	const AccessPointScanner *ap_scanner_ = nullptr;
+};
+
+#endif // ACCESS_POINT_DETAIL_TREE_H
