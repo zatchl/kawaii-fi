159225860483cd6cb7d82eb0c436fac96f2d1b7f
Zach Leytus <zach.leytus@pm.me>
2019-08-28 18:37:39 -0700

    Parse the fields of the WPA IE

--- libkawaii-fi/include/libkawaii-fi/ies/wpa.h
+++ libkawaii-fi/include/libkawaii-fi/ies/wpa.h
@@ -1,13 +1,28 @@
 #ifndef WPA_H
 #define WPA_H
 
+#include "libkawaii-fi/security.h"
 #include "vendor_specific.h"
 
+#include <QVector>
 #include <array>
 
 class Wpa : public VendorSpecific {
+public:
 	Wpa() = default;
 	Wpa(const VendorSpecific &vs);
+
+	[[nodiscard]] unsigned int version() const;
+	[[nodiscard]] CipherSuite group_cipher_suite() const;
+	[[nodiscard]] unsigned int pairwise_cipher_suite_count() const;
+	[[nodiscard]] QVector<CipherSuite> pairwise_cipher_suites() const;
+	[[nodiscard]] unsigned int akm_suite_count() const;
+	[[nodiscard]] QVector<AkmSuite> akm_suites() const;
+
+private:
+	[[nodiscard]] unsigned int pairwise_cipher_suites_start() const;
+	[[nodiscard]] unsigned int akm_suites_start() const;
+	[[nodiscard]] unsigned int wpa_capabilities_start() const;
 };
 
 inline const std::array<unsigned char, 3> WPA_OUI = {0x00, 0x50, 0xf2};
--- libkawaii-fi/src/ies/wpa.cpp
+++ libkawaii-fi/src/ies/wpa.cpp
@@ -2,4 +2,120 @@
 
 #include "libkawaii-fi/ies/vendor_specific.h"
 
+#include <QByteArray>
+#include <cstdint>
+
 Wpa::Wpa(const VendorSpecific &vs) : VendorSpecific(vs) {}
+
+unsigned int Wpa::version() const
+{
+	if (bytes().size() < 6) {
+		return 1;
+	}
+	return static_cast<std::uint8_t>(bytes()[4] + (bytes()[5] << 8));
+}
+
+CipherSuite Wpa::group_cipher_suite() const
+{
+	if (bytes().size() < 10) {
+		return CipherSuite();
+	}
+	std::array<std::uint8_t, 3> oui = {static_cast<std::uint8_t>(bytes()[6]),
+	                                   static_cast<std::uint8_t>(bytes()[7]),
+	                                   static_cast<std::uint8_t>(bytes()[8])};
+	switch (bytes()[9]) {
+	case 1:
+		return {oui, CipherSuiteType::WEP40};
+	case 2:
+		return {oui, CipherSuiteType::TKIP};
+	case 5:
+		return {oui, CipherSuiteType::WEP104};
+	default:
+		return {oui, CipherSuiteType::WEP40};
+	}
+}
+
+unsigned int Wpa::pairwise_cipher_suite_count() const
+{
+	if (static_cast<unsigned int>(bytes().size()) < pairwise_cipher_suites_start()) {
+		return 0;
+	}
+	return static_cast<unsigned int>(bytes()[10] + (bytes()[11] << 8));
+}
+
+QVector<CipherSuite> Wpa::pairwise_cipher_suites() const
+{
+	QVector<CipherSuite> cipher_suites;
+	if (static_cast<unsigned int>(bytes().size()) <
+	    (pairwise_cipher_suites_start() + pairwise_cipher_suite_count() * 4)) {
+		return cipher_suites;
+	}
+	auto it = bytes().begin() + pairwise_cipher_suites_start();
+	const auto end_it = it + pairwise_cipher_suite_count() * 4;
+	while (it < end_it) {
+		std::array<std::uint8_t, 3> oui = {static_cast<std::uint8_t>(*it),
+		                                   static_cast<std::uint8_t>(*(it + 1)),
+		                                   static_cast<std::uint8_t>(*(it + 2))};
+		switch (*(it + 3)) {
+		case 0:
+			cipher_suites.append({oui, CipherSuiteType::SameAsGroupCipher});
+			break;
+		case 1:
+			cipher_suites.append({oui, CipherSuiteType::WEP40});
+			break;
+		case 2:
+			cipher_suites.append({oui, CipherSuiteType::TKIP});
+			break;
+		case 5:
+			cipher_suites.append({oui, CipherSuiteType::WEP104});
+			break;
+		}
+		it += 4;
+	}
+	return cipher_suites;
+}
+
+unsigned int Wpa::akm_suite_count() const
+{
+	if (static_cast<unsigned int>(bytes().size()) < akm_suites_start()) {
+		return 0;
+	}
+	return static_cast<unsigned int>(bytes()[8 + (pairwise_cipher_suite_count() * 4)] +
+	                                 (bytes()[8 + (pairwise_cipher_suite_count() * 4) + 1] << 8));
+}
+
+QVector<AkmSuite> Wpa::akm_suites() const
+{
+	QVector<AkmSuite> akm_suites;
+	if (static_cast<unsigned int>(bytes().size()) < wpa_capabilities_start()) {
+		return akm_suites;
+	}
+	auto it = bytes().begin() + akm_suites_start();
+	const auto end_it = it + akm_suite_count() * 4;
+	while (it < end_it) {
+		std::array<unsigned char, 3> oui = {static_cast<unsigned char>(*it),
+		                                    static_cast<unsigned char>(*(it + 1)),
+		                                    static_cast<unsigned char>(*(it + 2))};
+		switch (*(it + 3)) {
+		case 1:
+			akm_suites.append({oui, AkmSuiteType::IEEE_8021X});
+			break;
+		case 2:
+			akm_suites.append({oui, AkmSuiteType::PSK});
+		}
+		it += 4;
+	}
+	return akm_suites;
+}
+
+unsigned int Wpa::pairwise_cipher_suites_start() const { return 12; }
+
+unsigned int Wpa::akm_suites_start() const
+{
+	return pairwise_cipher_suites_start() + (pairwise_cipher_suite_count() * 4) + 2;
+}
+
+unsigned int Wpa::wpa_capabilities_start() const
+{
+	return akm_suites_start() + (akm_suite_count() * 4);
+}
