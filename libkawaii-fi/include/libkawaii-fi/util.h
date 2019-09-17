8ab7cd3ef584171e366e28139a91afb500db0922
Zach Leytus <zach.leytus@pm.me>
2019-09-17 02:56:22 -0700

    Use unsigned int for channel conversion functions

--- libkawaii-fi/include/libkawaii-fi/util.h
+++ libkawaii-fi/include/libkawaii-fi/util.h
@@ -4,30 +4,44 @@
 #include <linux/nl80211.h>
 
 // From linux/net/wireless/util.c
-inline int ieee80211_frequency_to_channel(int freq)
+inline constexpr unsigned int ieee80211_frequency_to_channel(unsigned int freq)
 {
 	/* see 802.11 17.3.8.3.2 and Annex J */
-	if (freq == 2484)
+	constexpr unsigned int channel_14_freq = 2484;
+	if (freq == channel_14_freq) {
 		return 14;
-	else if (freq < 2484)
+	}
+
+	if (freq < channel_14_freq) {
 		return (freq - 2407) / 5;
-	else if (freq >= 4910 && freq <= 4980)
+	}
+
+	constexpr unsigned int four_point_nine_ghz_min = 4910;
+	constexpr unsigned int four_point_nine_ghz_max = 4980;
+	if (freq >= four_point_nine_ghz_min && freq <= four_point_nine_ghz_max) {
 		return (freq - 4000) / 5;
-	else if (freq <= 45000) /* DMG band lower limit */
+	}
+
+	constexpr unsigned int dmg_band_min = 45000;
+	if (freq <= dmg_band_min) {
 		return (freq - 5000) / 5;
-	else if (freq >= 58320 && freq <= 70200)
+	}
+
+	constexpr unsigned int sixty_ghz_min = 58320;
+	constexpr unsigned int sixty_ghz_max = 70200;
+	if (freq >= sixty_ghz_min && freq <= sixty_ghz_max) {
 		return (freq - 56160) / 2160;
-	else
-		return 0;
+	}
+
+	return 0;
 }
 
 // From linux/net/wireless/util.c
-inline int ieee80211_channel_to_frequency(int chan, enum nl80211_band band)
+inline constexpr unsigned int ieee80211_channel_to_frequency(unsigned int chan,
+                                                             enum nl80211_band band)
 {
 	/* see 802.11 17.3.8.3.2 and Annex J
 	 * there are overlapping channel numbers in 5GHz and 2GHz bands */
-	if (chan <= 0)
-		return 0; /* not supported */
 	switch (band) {
 	case NL80211_BAND_2GHZ:
 		if (chan == 14)
@@ -36,14 +50,14 @@ inline int ieee80211_channel_to_frequency(int chan, enum nl80211_band band)
 			return 2407 + chan * 5;
 		break;
 	case NL80211_BAND_5GHZ:
-		if (chan >= 182 && chan <= 196)
+		if (chan >= 182 && chan <= 196) {
 			return 4000 + chan * 5;
-		else
-			return 5000 + chan * 5;
-		break;
+		}
+		return 5000 + chan * 5;
 	case NL80211_BAND_60GHZ:
-		if (chan < 7)
+		if (chan < 7) {
 			return 56160 + chan * 2160;
+		}
 		break;
 	default:;
 	}
