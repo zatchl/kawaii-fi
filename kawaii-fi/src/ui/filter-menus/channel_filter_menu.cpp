5409ae37e3e3a7dd9fdee00be52caaa883f5f5ff
Zach Leytus <zach.leytus@pm.me>
2019-09-17 23:21:43 -0700

    Replace channel numbers with named constants

--- kawaii-fi/src/ui/filter-menus/channel_filter_menu.cpp
+++ kawaii-fi/src/ui/filter-menus/channel_filter_menu.cpp
@@ -2,6 +2,7 @@
 
 #include "ui/filter-menus/access_point_filter_menu.h"
 
+#include <QSet>
 #include <QString>
 #include <QVariant>
 #include <QVector>
@@ -12,34 +13,47 @@ class QWidget;
 ChannelFilterMenu::ChannelFilterMenu(QWidget *parent) : AccessPointFilterMenu("Channel", parent)
 {
 	QMenu *two_point_four_menu = addMenu("2.4 GHz");
-	for (unsigned int i = 1; i <= 14; ++i) {
+	constexpr unsigned int min_two_point_four_channel = 1;
+	constexpr unsigned int max_two_point_four_channel = 14;
+	for (unsigned int i = min_two_point_four_channel; i <= max_two_point_four_channel; ++i) {
 		add_filter_check_box(two_point_four_menu, QString::number(i), i);
 	}
 
 	QMenu *unii_1_menu = addMenu("5 GHz U-NII-1");
-	for (unsigned int i = 32; i <= 48; i += 2) {
+	constexpr unsigned int min_unii_1_channel = 32;
+	constexpr unsigned int max_unii_1_channel = 48;
+	for (unsigned int i = min_unii_1_channel; i <= max_unii_1_channel; i += 2) {
 		add_filter_check_box(unii_1_menu, QString::number(i), i);
 	}
 
 	QMenu *unii_2a_menu = addMenu("5 GHz U-NII-2A");
-	for (unsigned int i = 50; i <= 68; i += 2) {
-		if (i == 66) {
+	constexpr unsigned int min_unii_2a_channel = 50;
+	constexpr unsigned int max_unii_2a_channel = 68;
+	const QSet<unsigned int> unii_2a_invalid_channels = {66};
+	for (unsigned int i = min_unii_2a_channel; i <= max_unii_2a_channel; i += 2) {
+		if (unii_2a_invalid_channels.contains(i)) {
 			continue;
 		}
 		add_filter_check_box(unii_2a_menu, QString::number(i), i);
 	}
 
 	QMenu *unii_2c_menu = addMenu("5 GHz U-NII-2C");
-	for (unsigned int i = 96; i <= 144; i += 2) {
-		if ((i == 98) || (i == 130)) {
+	constexpr unsigned int min_unii_2c_channel = 96;
+	constexpr unsigned int max_unii_2c_channel = 144;
+	const QSet<unsigned int> unii_2c_invalid_channels = {98, 130};
+	for (unsigned int i = min_unii_2c_channel; i <= max_unii_2c_channel; i += 2) {
+		if (unii_2c_invalid_channels.contains(i)) {
 			continue;
 		}
 		add_filter_check_box(unii_2c_menu, QString::number(i), i);
 	}
 
 	QMenu *unii_3_menu = addMenu("5 GHz U-NII-3");
-	for (unsigned int i = 149; i <= 173; i += 2) {
-		if ((i == 163) || (i == 167) || (i == 171)) {
+	constexpr unsigned int min_unii_3_channel = 149;
+	constexpr unsigned int max_unii_3_channel = 173;
+	const QSet<unsigned int> unii_3_invalid_channels = {163, 167, 171};
+	for (unsigned int i = min_unii_3_channel; i <= max_unii_3_channel; i += 2) {
+		if (unii_3_invalid_channels.contains(i)) {
 			continue;
 		}
 		add_filter_check_box(unii_3_menu, QString::number(i), i);
